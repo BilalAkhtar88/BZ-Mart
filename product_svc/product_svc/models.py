5c2c357f1d03c437a69442426901117a3083c02a
BilalAkhtar88 <bilal_akhtar88@yahoo.com>
2024-07-15 16:18:18 +0500

    p

--- product_svc/product_svc/models.py
+++ product_svc/product_svc/models.py
@@ -9,11 +9,14 @@ class Product (BaseModel):
     description: str
     price: float
     category: str
+    operation: str
 
 
 class ProductUpdate(BaseModel):
+    id: Optional[str] = None
     product_id: Optional[str] = None
     name: Optional[str] = None
     description: Optional[str] = None
     price: Optional[float] = None
     category: Optional[str] = None
+    operation: Optional[str] = None
