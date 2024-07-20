d9b72fe5383c65ff4994ac04b682b01044035b55
BilalAkhtar88 <bilal_akhtar88@yahoo.com>
2024-07-21 00:20:15 +0500

    p

--- user_svc/user_svc/settings.py
+++ user_svc/user_svc/settings.py
@@ -10,5 +10,5 @@ except FileNotFoundError:
 DATABASE_URL = config("DATABASE_URL", cast=Secret)
 TEST_DATABASE_URL = config("TEST_DATABASE_URL", cast=Secret)
 # BOOTSTRAP_SERVER = config("BOOTSTRAP_SERVER", cast=str)
-KAFKA_USER_TOPIC = config("KAFKA_USER_TOPIC", cast=str)
+# KAFKA_USER_TOPIC = config("KAFKA_USER_TOPIC", cast=str)
 # KAFKA_CONSUMER_GROUP_ID = config("KAFKA_CONSUMER_GROUP_ID", cast=str)
